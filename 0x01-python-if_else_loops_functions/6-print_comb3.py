1a1e403c259243679a7d4ee890569d255d63791e
Grey-What <greywhat2002@gmail.com>
2024-01-03 03:38:21 -0800

    script prints combination of two numbers within a range

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# Write a program that prints all possible different combinations of two digits.
+
+for digit1 in range(0, 8):
+    for digit2 in range(digit1 + 1, 10):
+        print("{:d}{:d}".format(digit1, digit2), end=", ")
+print("{:d}{:d}".format(digit1 + 1, digit2))
