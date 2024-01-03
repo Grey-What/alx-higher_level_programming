38ed9cabb9c751c612150cf71b9439f7ec8c6ce8
Grey-What <greywhat2002@gmail.com>
2024-01-03 03:07:50 -0800

    pycodestyle fix

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-#prints the ASCII alphabet in lowercase, not followed by a new line
-
+# prints the ASCII alphabet in lowercase, not followed by a new line
 for lett in range(97, 123):
     print("{}".format(chr(lett)), end='')
