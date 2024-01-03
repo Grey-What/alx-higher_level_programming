2f867d99c8b5e5cc05d7e051aa41201ac87fcac3
Grey-What <greywhat2002@gmail.com>
2024-01-03 03:05:24 -0800

    fixed syntax issue

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -2,4 +2,4 @@
 #prints the ASCII alphabet in lowercase, not followed by a new line
 
 for lett in range(97, 123):
-    print("{}".format(chr(lett), end=""))
+    print("{}".format(chr(lett)), end='')
