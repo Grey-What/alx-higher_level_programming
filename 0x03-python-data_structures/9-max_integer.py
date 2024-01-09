018840cab3d1ac4151d67f7a8830a6e691ddc219
Grey-What <greywhat2002@gmail.com>
2024-01-09 06:26:39 -0800

    changed none to string

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -5,7 +5,7 @@ def max_integer(my_list=[]):
     max_int = 0
 
     if len(my_list) == 0:
-        return None
+        return "None"
     for i in my_list:
         if i > max_int:
             max_int = i
