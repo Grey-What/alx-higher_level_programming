eca58a8012565b5f5ab0cbb0b5e426f63d7304ca
Grey-What <greywhat2002@gmail.com>
2024-01-09 06:30:14 -0800

    changed interation of list so integers will be evaluated

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,11 +2,11 @@
 # function finds biggest integer in a list
 
 def max_integer(my_list=[]):
-    max_int = 0
+    max_int = my_list[0]
 
     if len(my_list) == 0:
-        return "None"
-    for i in my_list:
-        if i > max_int:
-            max_int = i
+        return None
+    for i in range(len(my_list)):
+        if my_list[i] > max_int:
+            max_int = my_list[i]
     return max_int
