c96c33aac3822fdfff4d7186195609830576283a
Grey-What <greywhat2002@gmail.com>
2024-01-10 04:22:05 -0800

    function replaces all occurens of an element by another in a new list

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""function replaces all occurences of a an element by another in a new list"""
+
+
+
+def search_replace(my_list, search, replace):
+    new_list = []
+
+    for element in my_list:
+        if element == search:
+            new_list.append(replace)
+        else:
+            new_list.append(element)
+    return new_list
