7050454892745419f4cbcc73566f30d319ed621f
Grey-What <greywhat2002@gmail.com>
2024-01-22 15:33:03 +0000

    prints int and handle exception of not int

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" safe_print_integer - prints an integer and handle exception"""
+
+
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except ValueError:
+        return (False)
+
