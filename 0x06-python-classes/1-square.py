84551145707e9c7fb188260db3435fd088b5d4d6
Grey-What <greywhat2002@gmail.com>
2024-01-24 09:41:56 +0000

    a class Square that defines the size of it

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Defines a Class Square"""
+
+
+class Square:
+    """Represent a Square with a size"""
+
+    def __init__(self, size):
+        """Initializes a new square
+
+        Args:
+        size (int): the size of a square created
+        """
+        self.__size = size
