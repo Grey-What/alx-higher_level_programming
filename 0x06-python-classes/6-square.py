e82bb5f57d29edbc46c1e67f86310f18963c5516
Grey-What <greywhat2002@gmail.com>
2024-01-24 13:25:54 +0000

    fixed output being printed

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -38,8 +38,10 @@ class Square:
     @position.setter
     def position(self, value):
         """setter for private attribute position"""
-        if not isinstance(value, tuple) or len(value) != 2 or
-        not all(i >= 0 for i in value):
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(i, int) for i in value) or
+                not all(i >= 0 for i in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
@@ -52,6 +54,7 @@ class Square:
         if self.__size == 0:
             print("")
 
+        [print("") for i in range(0, self.__position[1])]
         for i in range(0, self.__size):
             [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
