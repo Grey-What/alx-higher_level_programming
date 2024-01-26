46b865b9f0f8b208292e23532ab6afcc1a5142aa
Grey-What <greywhat2002@gmail.com>
2024-01-26 14:38:38 +0000

    changed order of if statements

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,11 +3,12 @@
 
 def fizzbuzz():
     for i in range(1, 101):
-        if i % 5 == 0 and i % 3 == 0:
+        if i % 3 == 0 and i % 5 == 0:
             print("Fizzbuzz ", end="")
-        elif i % 5 == 0:
-            print("Buzz ", end="")
         elif i % 3 == 0:
             print("Fizz ", end="")
+        elif i % 5 == 0:
+            print("Buzz ", end="")
         else:
-            print(str(i) + " ", end="")
+            print("{} ".format(i), end="")
+
