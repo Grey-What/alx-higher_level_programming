7b0d82764b34c0368b4265863d48e5edabdc425b
Grey-What <greywhat2002@gmail.com>
2024-01-26 14:10:15 +0000

    change all lowercase characters to uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""print a string in uppercase"""
+
+
+def uppercase(str):
+    for lett in str:
+        if ord(lett) >= 97 and ord(lett) <= 122:
+            lett = chr(ord(lett) - 32)
+        print("{}".format(lett), end="")
+    print("")
