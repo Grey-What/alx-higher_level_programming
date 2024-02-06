43824f1f682cffc70828e4076e6f919b65dc75de
Grey-What <greywhat2002@gmail.com>
2024-02-06 14:38:23 +0000

    added public instance method that raise exception with message

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Empty class BaseGeometry"""
+
+
+class BaseGeometry:
+    """represents a class"""
+
+    def area(self):
+        """raise exception that area is not implimented"""
+        raise Exception("area() is not implemented")
