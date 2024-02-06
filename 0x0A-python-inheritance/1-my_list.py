1526828a89d38c75d5a9cf5cf4207767eef56f2e
Grey-What <greywhat2002@gmail.com>
2024-02-06 12:32:42 +0000

    subclass that inherits from list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Class that inherits from list"""
+
+
+class MyList(list):
+    """inherits from list"""
+    def __init__(self):
+        """instatination of instance"""
+        super().__init__()
+
+    def print_sorted(self):
+        """prints the sorted list"""
+        self.sort()
+        print(self)
