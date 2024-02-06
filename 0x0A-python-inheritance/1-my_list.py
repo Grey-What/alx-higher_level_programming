ffafbbb6a02af3eff118afafa85ad95258621bd7
Grey-What <greywhat2002@gmail.com>
2024-02-06 13:25:21 +0000

    removed __init__

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,10 +6,6 @@ Class that inherits from list
 
 class MyList(list):
     """inherits from list"""
-    def __init__(self):
-        """instatination of instance"""
-        super().__init__()
-
     def print_sorted(self):
         """prints the sorted list"""
         print(sorted(self))
