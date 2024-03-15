9e11dc28d823f4ba2c55b5ca4bac78d5df8d3e8a
Grey-What <greywhat2002@gmail.com>
2024-03-15 00:16:35 -0700

    removed database name from query

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,7 +1,4 @@
 -- list all cities of california in database hbtn_0d_usa
-SELECT id, name
-FROM hbtn_0d_usa.cities
-WHERE state_id = (SELECT id
-      	       	  FROM hbtn_0d_usa.states
-		  WHERE name = 'California')
-ORDER BY (cities.id) ASC;
+SELECT id, name FROM cities WHERE state_id =
+(SELECT id FROM states WHERE name = 'California')
+ORDER BY (id);
