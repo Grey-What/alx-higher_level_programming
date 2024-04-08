06057cddaac77093d1a4fdc5f91e68a40b72d29e
Nick <greywhat2002@gmail.com>
2024-04-08 08:49:13 -0700

    script

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// print strings from array with loop
+const myStrings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i = 0;
+
+while (myStrings[i]) {
+  console.log(myStrings[i]);
+  i++;
+}
