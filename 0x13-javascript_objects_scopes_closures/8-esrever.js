246aaadf8933b7487629823194c3f15befe693d0
Nick <greywhat2002@gmail.com>
2024-04-09 06:43:13 -0700

    fixed camelcase

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,7 +2,7 @@
 // returns reversed version of a list
 
 exports.esrever = function (list) {
-  const reverse_list = [];
+  const reverseList = [];
 
   for (let i = list.length - 1; i >= 0; i--) {
     reverse_list.push(list[i]);
