36bf52b89287f5c6c76089fc6c620e463c31c6d3
Nick <greywhat2002@gmail.com>
2024-04-11 04:54:53 -0700

    retrieves all states from database using MySQLdb

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" list all states from database hbtn_0e_0_usa """
+
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    """
+    Access database and get names of states stored in it
+    """
+    db = MySQLdb.connect(
+        host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states")
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
