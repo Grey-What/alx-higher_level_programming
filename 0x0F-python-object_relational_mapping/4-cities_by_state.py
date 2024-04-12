e73594e9edc0ad3cfb3cf705f7865f81444a71a7
Nick <greywhat2002@gmail.com>
2024-04-12 01:36:27 -0700

    script list all cities in database

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+list all 'cities' from database 'hbtn_0e_4_usa'
+sorted in ascending order by id
+"""
+
+import MySQLdb as dbmod
+from sys import argv
+
+if __name__ == "__main__":
+    """list all cities from database"""
+
+    db = dbmod.connect(host="localhost", port=3306, user=argv[1],
+                       passwd=argv[2], db=argv[3])
+
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM cities ORDER BY cities.id ASC")
+
+    result = cur.fetchall()
+
+    for row in result:
+        print(row)
