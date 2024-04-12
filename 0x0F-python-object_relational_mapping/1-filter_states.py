1d78fa017f0ecb5e3e512a78d05180b012b32666
Nick <greywhat2002@gmail.com>
2024-04-12 00:51:23 -0700

    fixed database

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,8 +8,8 @@ import sys
 if __name__ == "__main__":
     """get state names that start with capital N"""
 
-    db = mySQL.connect(
-        host="localhost", user=sys.argv[1], port=3306,
+    db = MySQLdb.connect(
+        host="localhost", port=3306, user=sys.argv[1],
         passwd=sys.argv[2], db=sys.argv[3])
 
     cur = db.cursor()
