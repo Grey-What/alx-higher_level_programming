2b25db6786c04e837ec9d0f9bbc4019898f2fd9f
grey <greywaht2002@gmail.com>
2024-04-26 03:23:10 -0700

    fix

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
-"""send post request to url with an email as parameter"""
+"""
+send post request to url with an email as parameter
+Display body of response
+"""
 
 import urllib.request
 import urllib.parse
 import sys
 
 
-url = sys.argv[1]
-email = {"email": str(sys.argv[2])}
-data = urllib.parse.urlencode(email)
-data = data.encode("ascii")
-req = urllib.request.Request(url)
-with urllib.request.urlopen(req) as res:
-    text = res.read()
-    print(text.decode("utf-8"))
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = {"email": str(sys.argv[2])}
+    data = urllib.parse.urlencode(email)
+    data = data.encode("ascii")
+
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as res:
+        text = res.read()
+        print(text.decode("utf-8"))
