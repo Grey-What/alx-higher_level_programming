7518c3985d640719103269d35498793494ce3992
grey <greywaht2002@gmail.com>
2024-04-26 05:47:34 -0700

    post request to url and evaluate if response body is valid json

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+takes a letter and url as argument
+sent post request to url with letter as parameter
+evaluate response to json file and print formated output
+"""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+
+    try:
+        q = sys.argv[1]
+    except IndexError:
+        q = ""
+
+    payload = {'q': q}
+
+    r = requests.post(url, data=payload)
+
+    try:
+        string = r.json()
+    except requests.exceptions.JSONDecodeError as e:
+        print("Not a valid JSON")
+
+    if len(string) != 0:
+        print("[{}] {}".format(string.get("id"), string.get("name")))
+    else:
+        print("No result")
