d62bb0d6554f4511f1d7e84ce87e158040f91864
grey <greywaht2002@gmail.com>
2024-04-26 04:54:08 -0700

    get url with requests pacakge

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+get url with package 'requests'
+Display body in specified format
+"""
+
+import requests
+
+
+if __name__ == "__main__":
+    r = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
