c3245ab76967524916b264e5bf0dd11968fbf736
Nick <greywhat2002@gmail.com>
2024-04-26 09:58:15 +0000

    sent request to url and extract header variable

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""accepts url and send request, then display value of header variable"""
+
+import urllib.request as urlreq
+import urllib.response as urlres
+import sys
+
+
+req = urlreq.Request(sys.argv[1])
+with urlreq.urlopen(req) as res:
+    print(res.info()["X-Request-Id"])
