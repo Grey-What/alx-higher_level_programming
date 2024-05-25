019e410ae54245cce95b5797c45b1c09f3816f38
GreyWhat <greywhat2002@gmail.com>
2024-05-25 03:02:46 -0700

    change in logic

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,7 +2,8 @@
 
 const request = require('request');
 const apiUrl = process.argv[2];
-const characterUrl = 'https://swapi-api.alx-tools.com/api/people/18/';
+const characterId = 18;
+let count = 0;
 const options = {
   url: `${apiUrl}`,
   json: true
@@ -12,12 +13,13 @@ request.get(options, (error, response, body) => {
   if (error) {
     console.log(error);
   } else {
-    let count = 0;
-    for (let i = 0; i < body.results.length; i++) {
-      if (body.results[i].characters.includes(characterUrl)) {
-        count++;
-      }
-    }
+    body.results.forEach((film) => {
+      film.characters.forEach((character) => {
+        if (character.includes(characterId)) {
+          count++;
+        }
+      });
+    });
     console.log(count);
   }
 });
