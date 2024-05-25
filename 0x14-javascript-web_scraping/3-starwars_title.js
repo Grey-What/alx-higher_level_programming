6fbfa66e1713529bb57c96e03216b0b2fe20d620
GreyWhat <greywhat2002@gmail.com>
2024-05-25 02:06:30 -0700

    using Star wars API

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const request = require('request');
+const movieId = process.argv[2];
+
+const options = {
+    url: `https://swapi-api.alx-tools.com/api/films/${movieId}`,
+    json: true
+};
+
+request.get(options, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(body.title);
+  }
+});
